2a22d34352b7fbe9b89561740a7e708bd3d7b3eb
Vimulatus <vasu.mit2501@gmail.com>
2024-08-24 01:49:07 +0530

    style(ui): update chat-bubble variant and add time to chat-bubble

--- packages/ui/src/chat-bubble.tsx
+++ packages/ui/src/chat-bubble.tsx
@@ -3,21 +3,17 @@ import { cn } from "@vim/ui/lib/utils";
 import React from "react";
 
 const chatBubbleVariants = cva(
-  "max-w-lg w-fit p-4 m-4 rounded-xl relative inline-flex justify-center items-center ",
+  "max-w-lg w-fit p-4 mx-2 first:mt-2 rounded-xl relative inline-flex justify-center items-center gap-4",
   {
     variants: {
       variant: {
-        primary: "bg-blue-500 text-white border-blue-500",
-        secondary: "bg-gray-300 text-accent-foreground border-gray-300",
-      },
-      position: {
-        right:
-          "pl-3 pr-5 place-self-end after:content-[''] after:absolute after:bg-background after:h-full after:w-6 after:rounded-bl-full rounded-r-none after:-right-4 border-e-8",
-        left: "place-self-start pl-6 before:content-[''] before:absolute before:bg-background before:h-full before:w-6 before:-left-4 before:rounded-br-full border-s-8 rounded-l-none",
+        sent: "bg-primary text-white border-primary pl-3 pr-5 place-self-end after:content-[''] after:absolute after:bg-background after:h-full after:w-6 after:rounded-bl-full rounded-r-none after:-right-4 border-e-8",
+        received:
+          "bg-secondary text-accent-foreground border-secondary place-self-start pl-6 before:content-[''] before:absolute before:bg-background before:h-full before:w-6 before:-left-4 before:rounded-br-full border-s-8 rounded-l-none",
       },
     },
     defaultVariants: {
-      variant: "primary",
+      variant: "sent",
     },
   },
 );
@@ -26,20 +22,24 @@ type ChatBubbleVariants = VariantProps<typeof chatBubbleVariants>;
 
 interface ChatBubbleProps
   extends React.HTMLAttributes<HTMLDivElement>,
-    ChatBubbleVariants {
+  ChatBubbleVariants {
   content: string;
   className?: string;
+  time?: Date;
 }
 
 const ChatBubble = React.forwardRef<HTMLDivElement, ChatBubbleProps>(
-  ({ content, position, className, variant, ...props }, ref) => {
+  ({ content, className, variant, time, ...props }, ref) => {
     return (
       <div
-        className={cn(chatBubbleVariants({ variant, position, className }))}
+        className={cn(chatBubbleVariants({ variant, className }))}
         ref={ref}
         {...props}
       >
-        {content}
+        <p>{content}</p>
+        <p className="place-self-end text-xs text-muted-foreground">
+          {time?.toString() ?? "10:00"}
+        </p>
       </div>
     );
   },
